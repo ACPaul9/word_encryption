3447eb757503fac7164e97e16efe141d5cac51cc
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-04-30 21:23:56 +0300

    version3

--- main.rb
+++ main.rb
@@ -8,11 +8,12 @@ puts <<~END
   Каким способом зашифровать:
   1. MD5
   2. SHA1
+  3. SHA2
 END
 
 user_choice = gets.to_i
 
-until user_choice.between?(1, 2)
+until user_choice.between?(1, 3)
   puts "Выберите 1 или 2"
   user_choice = STDIN.gets.to_i
 end
@@ -21,4 +22,5 @@ puts "Вот что получилось:"
 case user_choice
 when 1 then puts Digest::MD5.hexdigest(word)
 when 2 then puts Digest::SHA1.hexdigest(word)
+when 3 then puts Digest::SHA2.hexdigest(word)
 end
